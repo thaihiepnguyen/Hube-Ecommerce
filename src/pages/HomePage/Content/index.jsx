38076673d08fc006ca00e286a5a96fd18a6baef2
thaihiepnguyen <thaihiep232002@gmail.com>
2023-07-23 00:47:12 +0700

    HB-23890v4 fix layout

--- src/pages/HomePage/Content/index.jsx
+++ src/pages/HomePage/Content/index.jsx
@@ -3,9 +3,9 @@ import Slider from "./Slider";
 import Footer from "../../../components/Footer";
 const Content = () => {
   return (
-      <div className={styles.contentContainer}>
+    <div className={styles.contentContainer}>
+      <div className={styles.innerContent}>
         <Slider/>
-
         <h1>Danh Sách Các Thể Loại</h1>
         <div style={{height: 300, border: 1, borderColor: "black", borderStyle: "solid", margin: 10}}>
           #HB-40348: Design Card Component
@@ -35,8 +35,9 @@ const Content = () => {
           #HB-40348: Design Card Component
           <div>Dang Bao Chau</div>
         </div>
-        <Footer/>
       </div>
+      <Footer/>
+    </div>
   )
 }
 
