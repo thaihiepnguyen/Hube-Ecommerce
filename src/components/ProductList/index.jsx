01c5a74a92b1d4266c55717f3af87840edb53134
thaihiepnguyen <thaihiep232002@gmail.com>
2023-07-26 14:19:41 +0700

    fix scroll done

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -11,44 +11,44 @@ const ProductList = props => {
   return (
     <>
       <h2>{label}</h2>
-      <Carousel
-        itemClass={styles.carouselItem}
-        customRightArrow={<CustomRightArrow />}
-        customLeftArrow={<CustomLeftArrow />}
-        responsive={{
-          desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 5,
-            slidesToSlide: 5,
-          },
-          tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 3,
-            slidesToSlide: 3,
-          },
-          mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1,
-          },
-        }}
-      >
-        {data.map(item => (
-          <div key={item.id}>
-            <ProductCard
-              image={item.image}
-              name={item.name}
-              rate={item.rate}
-              price={item.price}
-              discount={item.discount}
-              address={item.address}
-              isFavorite={item.isFavorite}
-              categories={item.categories || [{name: 'iPhone'}, {name: 'Apple'}]}
-              shopName={item.shopName || 'Gojo Shop'}
-            />
-          </div>
-        ))}
-      </Carousel>
+        <Carousel
+          itemClass={styles.carouselItem}
+          customRightArrow={<CustomRightArrow />}
+          customLeftArrow={<CustomLeftArrow />}
+          responsive={{
+            desktop: {
+              breakpoint: { max: 3000, min: 1024 },
+              items: 5,
+              slidesToSlide: 1,
+            },
+            tablet: {
+              breakpoint: { max: 1024, min: 464 },
+              items: 3,
+              slidesToSlide: 3,
+            },
+            mobile: {
+              breakpoint: { max: 464, min: 0 },
+              items: 1,
+              slidesToSlide: 1,
+            },
+          }}
+        >
+          {data.map(item => (
+            <div key={item.id}>
+              <ProductCard
+                image={item.image}
+                name={item.name}
+                rate={item.rate}
+                price={item.price}
+                discount={item.discount}
+                address={item.address}
+                isFavorite={item.isFavorite}
+                categories={item.categories || [{name: 'iPhone'}, {name: 'Apple'}]}
+                shopName={item.shopName || 'Gojo Shop'}
+              />
+            </div>
+          ))}
+        </Carousel>
     </>
   );
 };
