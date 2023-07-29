cc20ba6b522e9260d958ecc4772a537f190fa1ef
PhongTran1804 <trannguyenphongg@gmail.com>
2023-07-29 10:34:40 +0700

    seller update 1.2

--- src/pages/Seller/Account/index.jsx
+++ src/pages/Seller/Account/index.jsx
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
 import styles from './styles.module.scss';
 import AccountItem from './AccountItem';
+import Avatar from './Avatar';
+import Logo from '../../../assets/hube_logo.png';
+
 
 const Account = () => {
     return (
         <div className={styles.container}>
             <div className={styles.title}>Account</div>
+            <Avatar imgURI={Logo} />
             <AccountItem title="Account" content="trannguyenphongg@gmail.com"/>
             <AccountItem title="ID Account" content="00001"/>
             <AccountItem title="Password" content="goodseller"/>
