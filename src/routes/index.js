5b3e6696fb7e852f3246d0ab5294e6c8500e9e86
ggj-user} <ggj-mail>
2023-08-13 08:52:05 +0700

    fix conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import LoginPage from '../pages/Login';
 import AdminPage from '../pages/Admin';
 import ProfilePage from "../pages/ProfilePage";
 import ShoppingCartPage from "../pages/ShoppingCartPage";
+import Seller from '../pages/Seller';
 import ProductDetailPage from "../pages/ProductDetailPage";
 import SearchPage from '../pages/SearchPage';
 
@@ -79,7 +80,16 @@ const router = createBrowserRouter([
       {
         path: 'discounts',
         element: <AdminPage />,
-      },
+      }
+    ],
+  },
+  {
+    path: '/seller',
+    children: [
+      {
+        path: '',
+        element: <Seller />,
+      }
     ],
   },
 ]);
